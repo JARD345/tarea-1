c24a74222ec40ad1da31e15ffbd109dd38af7780
JARD345 <160336763+JARD345@users.noreply.github.com>
2024-02-23 20:16:50 -0600

    Add files via upload

---
+++ mayor_de_edad/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int edad;
+    cout<<"ingresa tu edad"<<endl;
+    cin>>edad;
+    if (edad>=18){
+        cout<<"Eres mayor de edad"<<endl;
+    }else{
+        cout<<"Eres menor de edad"<<endl;
+    }
+    return 0;
+}
